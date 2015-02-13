7c3c7daf1dd45bbdadf13d3d70a0633a32780acd
Combro2k <martijn@vmaurik.nl>
2015-02-13 10:44:34 +0100

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,16 +2,16 @@
 
 echo "Starting setup..."
 
-/usr/local/sbin/openvas-mkcert -q
+/usr/sbin/openvas-mkcert -q
 ldconfig
-/usr/local/sbin/openvassd
-/usr/local/sbin/openvas-nvt-sync
-/usr/local/sbin/openvas-scapdata-sync
-/usr/local/sbin/openvas-certdata-sync
-/usr/local/sbin/openvas-mkcert-client -n -i
-/usr/local/sbin/openvasmd --rebuild
-/usr/local/sbin/openvasmd --create-user=admin --role=Admin
-/usr/local/sbin/openvasmd --user=admin --new-password=openvas
+/usr/sbin/openvassd
+/usr/sbin/openvas-nvt-sync
+/usr/sbin/openvas-scapdata-sync
+/usr/sbin/openvas-certdata-sync
+/usr/sbin/openvas-mkcert-client -n -i
+/usr/sbin/openvasmd --rebuild
+/usr/sbin/openvasmd --create-user=admin --role=Admin
+/usr/sbin/openvasmd --user=admin --new-password=openvas
 
 # At this point, usually openvassd locks up so lets kill it
 kill -9 $(pidof openvassd)
